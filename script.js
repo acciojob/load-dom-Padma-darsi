bd2563d35b0653bea3b476400fb476f5f6f4f045
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-01-05 20:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    // Create a new text node
+    const text = document.createTextNode("DOM load success");
+
+    // Append the text node to the body
+    document.body.appendChild(text);
+});
